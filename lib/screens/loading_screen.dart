69404ca6768e5bc3bc07fa23431a54e81204248d
Jin Ozono <ozononicholas@gmail.com>
2023-11-27 23:26:41 -0300

    jsondecode

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -30,7 +31,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+      //print(data);
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      print(temperature);
+
+      int conditionId = decodedData['weather'][0]['id'];
+      print(conditionId);
+
+      String cityName = decodedData['name'];
+      print(cityName);
     } else {
       print(response.statusCode);
     }
